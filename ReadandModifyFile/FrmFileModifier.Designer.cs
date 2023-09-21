308c22ed20263534705bddfb55b5731d4e1536b8
suli.jin <suli.jin@cloud.statcan.ca>
2023-09-21 09:17:54 -0400

    initial commit

---
+++ ReadandModifyFile/FrmFileModifier.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace ReadandModifyFile
+{
+    partial class FrmFileModifier
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnOpenFolder = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.btnModifyWrite = new System.Windows.Forms.Button();
+            this.percentageLabel = new System.Windows.Forms.Label();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.backgroundWorker = new System.ComponentModel.BackgroundWorker();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.SuspendLayout();
+            // 
+            // btnOpenFolder
+            // 
+            this.btnOpenFolder.Location = new System.Drawing.Point(11, 11);
+            this.btnOpenFolder.Margin = new System.Windows.Forms.Padding(2);
+            this.btnOpenFolder.Name = "btnOpenFolder";
+            this.btnOpenFolder.Size = new System.Drawing.Size(101, 20);
+            this.btnOpenFolder.TabIndex = 0;
+            this.btnOpenFolder.Text = "Browse Files";
+            this.btnOpenFolder.UseVisualStyleBackColor = true;
+            this.btnOpenFolder.Click += new System.EventHandler(this.btnSelectMultiple_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(8, 197);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(330, 27);
+            this.progressBar.TabIndex = 2;
+            this.progressBar.Visible = false;
+            // 
+            // btnModifyWrite
+            // 
+            this.btnModifyWrite.Enabled = false;
+            this.btnModifyWrite.Location = new System.Drawing.Point(465, 76);
+            this.btnModifyWrite.Name = "btnModifyWrite";
+            this.btnModifyWrite.Size = new System.Drawing.Size(101, 27);
+            this.btnModifyWrite.TabIndex = 3;
+            this.btnModifyWrite.Text = "Modify Files";
+            this.btnModifyWrite.UseVisualStyleBackColor = true;
+            this.btnModifyWrite.Click += new System.EventHandler(this.BtnModifyWrite_Click);
+            // 
+            // percentageLabel
+            // 
+            this.percentageLabel.AutoSize = true;
+            this.percentageLabel.Location = new System.Drawing.Point(375, 211);
+            this.percentageLabel.Name = "percentageLabel";
+            this.percentageLabel.Size = new System.Drawing.Size(23, 13);
+            this.percentageLabel.TabIndex = 4;
+            this.percentageLabel.Text = "%%";
+            this.percentageLabel.Visible = false;
+            this.percentageLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(8, 76);
+            this.textBox.Multiline = true;
+            this.textBox.Name = "textBox";
+            this.textBox.ReadOnly = true;
+            this.textBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox.Size = new System.Drawing.Size(403, 70);
+            this.textBox.TabIndex = 5;
+            this.textBox.TextChanged += new System.EventHandler(this.textBox_TextChanged);
+            // 
+            // backgroundWorker
+            // 
+            this.backgroundWorker.WorkerReportsProgress = true;
+            this.backgroundWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker_DoWork);
+            this.backgroundWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker_ProgressChanged);
+            this.backgroundWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker_RunWorkerCompleted);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // FrmFileModifier
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(629, 254);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.percentageLabel);
+            this.Controls.Add(this.btnModifyWrite);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.btnOpenFolder);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "FrmFileModifier";
+            this.Text = "FileModifier";
+            this.Load += new System.EventHandler(this.FrmFileModifier_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpenFolder;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Button btnModifyWrite;
+        private System.Windows.Forms.Label percentageLabel;
+        private System.Windows.Forms.TextBox textBox;
+        private System.ComponentModel.BackgroundWorker backgroundWorker;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+    }
+}
+
---
+++ ReadandModifyFile/FrmFileModifier.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReadandModifyFile
+{
+    public partial class FrmFileModifier : Form
+    {
+        string[] fileNames = Array.Empty<string>();
+        bool exceptionCatched;
+        public FrmFileModifier()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelectMultiple_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                selectMultipleFiles();
+            }
+            catch (ApplicationException ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            btnModifyWrite.Enabled = true;
+        }
+
+        private void BtnModifyWrite_Click(object sender, EventArgs e)
+        {
+            //if (String.IsNullOrEmpty(strFilePath))
+            //{
+            //    MessageBox.Show("No file loaded");
+            //    return;
+            //}
+            btnModifyWrite.Enabled = false;
+            progressBar.Visible = true;
+            percentageLabel.Visible = true;
+            btnOpenFolder.Enabled = false;
+            progressBar.Maximum = 100;
+            progressBar.Step = 1;
+            progressBar.Value = 0;
+            backgroundWorker.RunWorkerAsync();
+        }
+        public void selectMultipleFiles()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            string strSelectedFileNames = "";
+            openFileDialog.Multiselect = true;
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                fileNames = openFileDialog.FileNames;
+                foreach (string fileName in fileNames)
+                {
+                    strSelectedFileNames += fileName + Environment.NewLine;
+                }
+                textBox.Text = strSelectedFileNames;
+            }
+            //FolderBrowserDialog fbd = new FolderBrowserDialog();
+            //if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            //    // shows the path to the selected folder in the folder dialog
+            //    //MessageBox.Show(fbd.SelectedPath);
+            //textBox.Text = fbd.SelectedPath;
+            //DirectoryInfo di = new DirectoryInfo(fbd.SelectedPath);
+            //FileInfo[] fileNames = di.GetFiles("*.txt");
+            //foreach (string fileName in Directory.EnumerateFiles(fbd.SelectedPath, "*.txt"))
+            //{
+            //    string contents = File.ReadAllText(fileName);
+            //}
+
+
+            //OpenFileDialog openFileDialog = new OpenFileDialog();
+            //openFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+
+            //if (openFileDialog.ShowDialog() == DialogResult.OK)
+            //    try
+            //    {
+            //        strFilePath = openFileDialog.FileName;
+            //        textBox.Text = strFilePath;
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        throw new ApplicationException("Cannot open the file");
+            //    }
+        }
+        public void backgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            try
+            {
+                ReadNWriteFile(fileNames);
+            }
+            catch (ApplicationException ex)
+            {
+                MessageBox.Show(ex.ToString());
+                exceptionCatched = true;
+                //if (!string.IsNullOrEmpty(fileNewName))
+                //{
+                //    File.Delete(fileNewName);
+                //}
+            }
+        }
+
+        // GUI components are updated
+        public void backgroundWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar.Value = e.ProgressPercentage;
+            percentageLabel.Text = e.ProgressPercentage.ToString() + "%";
+            if (e.ProgressPercentage == 99)
+            {
+                exceptionCatched = false;
+            }
+        }
+
+        // Complete the work after DoWork and ProgressChanged.
+        public void backgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (exceptionCatched)
+            {
+                textBox.Clear();
+            }
+            else
+            {
+                string strFileDirectory = Path.GetDirectoryName(fileNames[0]);
+                Process.Start(strFileDirectory);
+                btnOpenFolder.Enabled = true;
+                textBox.Text = "";
+                this.Close();
+            }
+        }
+
+        public void ReadNWriteFile(string[] fNames)
+        {
+            int totalFileNumber = fNames.Length;
+            int fileNumCounter = 0;
+            int position;
+            string line = String.Empty;
+            string lineModified = String.Empty;
+            string fileNewName = String.Empty;
+
+            foreach (var fName in fNames)
+            {
+                try
+                {
+                    position = fName.LastIndexOf(".");
+                    fileNewName = fName.Insert(position, "_Modified");
+                    //line = String.Empty;
+                    //lineModified = String.Empty;
+                    //long totalLineCount = File.ReadLines(fName).Count();
+
+                    using (StreamReader streamReader = new StreamReader(fName))
+                    using (StreamWriter streamWriter = new StreamWriter(fileNewName))
+                    {
+                        while (!streamReader.EndOfStream)
+                        {
+                            line = streamReader.ReadLine();
+                            lineModified = line.Replace("|0x", "|");
+                            streamWriter.WriteLine(lineModified);
+                            //lineCounter++;
+                            //backgroundWorker.ReportProgress((int)(lineCounter * 100 / totalLineCount));
+                        }
+                    }
+                    fileNumCounter++;
+                    backgroundWorker.ReportProgress((fileNumCounter * 100 / totalFileNumber));
+                }
+                catch (Exception e)
+                {
+                    throw new ApplicationException("The file cannot be read/written.");
+                }
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FrmFileModifier_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox_TextChanged(object sender, EventArgs e)
+        {
+            textBox.SelectionStart = textBox.Text.Length;
+            textBox.ScrollToCaret();
+        }
+    }
+}
